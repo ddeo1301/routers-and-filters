3a846295567ad8e4a835c7d004ae612729be42da
ddeo1301 <divyanshudeo1301@gmail.com>
2023-04-29 12:32:12 +0530

    üse of the controller

--- routes/admin.js
+++ routes/admin.js
@@ -1,26 +1,12 @@
 const path = require('path');
 const express = require('express');
 
-const rootDir = require('../util/path');
+const productController=require('../controller/product');
+
 const router = express.Router();
 
-// app.use('/add-product', (req, res, next) => { 
-//     res.send('<form action="/product" method="POST"><input type="text" name="title"><br><input type="number" name="size"><br></r><button type="submit">Add Product</button></form>');
-//   });
-
-router.get('/add-product', (req, res, next) => { 
-    // res.send('<form action="/admin/add-product" method="POST">' +
-    //  '<input type="text" name="title"><br>' +
-    //  '<input type="number" name="size"><br>' +
-    //  '<button type="submit">Add Product</button></form>');
-   //res.sendFile(path.join(__dirname,'../', 'views', 'add-product.html'))
-   res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
-  });
-
-router.post('add-product', (req, res, next) => { 
-    console.log(req.body);
-    res.redirect('/');
-});
+router.get('/add-product',productController.getAddProduct);
+router.post('/add-product',productController.postAddproduct);
 
 module.exports = router;
 
